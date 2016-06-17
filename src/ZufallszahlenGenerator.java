b433c7c9de7ab0b686e3220e25888b0441772cba
Sonja Riethig <sriethig@googlemail.com>
2016-06-17 08:32:06 +0200

    Zufallszahlen done

--- src/ZufallszahlenGenerator.java
+++ src/ZufallszahlenGenerator.java
@@ -12,27 +12,31 @@ public class ZufallszahlenGenerator {
 		List<Integer> zufallsZahlen = new ArrayList<Integer>();
 		
 		HashMap<Integer, Integer> verteilung = new HashMap<Integer, Integer>();
-		for(int i = 0; i < 127; i++) {
+		for(int i = 1; i < 127; i++) {
 			verteilung.put(i, 1);
 		}
 		
 		int seed = input;
-		int m = 21454;
-		int factor = 1927843;
-		int additiveConst = 18237890;
+		int m = 2147483647;
+		int factor = 16807;
+		int additiveConst = 0;
 
 		for(int i = 0; i < 2000; i++) {
-			seed = (factor*seed + (additiveConst % m)) % 128;
+			seed = (factor*seed + (additiveConst % m)) % 127;
 			if(seed < 0) {
 				seed = seed * (-1);
 			}
-			zufallsZahlen.add(seed);
+			zufallsZahlen.add(seed + 1);
 			verteilung.put(seed, (verteilung.get(seed) + 1));
 		}
 		
-		for(int i = 0; i < 127; i++) {
+		for(int i = 1; i < 127; i++) {
 			System.out.println(i + ": " + verteilung.get(i));
 		}
+		
+		/*for(int i = 0; i < 127; i++) {
+			System.out.println(i + ": " + zufallsZahlen.get(i));
+		}*/
 
 		return zufallsZahlen;
 	}
